b52fad6dbb79fc2b19b871a9662d8a8413bf7183
Nazaret Román Guerrero <nazaret.ro.gue@gmail.com>
2023-12-13 23:15:17 +0100

    Fixed parenthesis

--- src/ECC/EllipticCurve.java
+++ src/ECC/EllipticCurve.java
@@ -15,42 +15,42 @@ class EllipticCurve {
 	}
 	
 	Point EllipticCurvePointAddition(Point point1, Point point2) {
-		if(point1 == null || point2 == null)
-			return null;
-		
-		if(point1.GetZ() == BigInteger.ZERO)
-			return point2;
-		
-		else if(point2.GetZ() == BigInteger.ZERO)
-			return point1;
-		
-		BigInteger lambda = BigInteger.ZERO;
+        if(point1 == null || point2 == null)
+            return null;
+        
+        if(point1.GetZ() == BigInteger.ZERO)
+            return point2;
+
+        else if(point2.GetZ() == BigInteger.ZERO)
+            return point1;
+        
+        BigInteger lambda = BigInteger.ZERO;
 		BigInteger numerator = BigInteger.ZERO;
 		BigInteger denominator = BigInteger.ZERO;
-		
+
 		if(point1.GetX() == point2.GetX()) {
-			BigInteger yAddition = (point1.GetY().add(point2.GetY())).mod(p);
-			
-			if(yAddition == BigInteger.ZERO)
-				return Point.INFINITY_POINT;
-			
-			numerator = (point1.GetX().multiply(point1.GetX()).multiply(new BigInteger("3"))).add(a);
-			denominator = point1.GetY().add(point1.GetY());
-		}
-		
-		else {
-			numerator = point2.GetY().add(point1.GetY().negate());
-			denominator = point2.GetX().add(point1.GetX().negate());
-		}
-		
-		lambda = numerator.multiply(denominator.modInverse(p));
-		BigInteger newXCoordinate = CalculateXCoordinate(lambda, point1, point2);
+        	BigInteger yAddition = (point1.GetY().add(point2.GetY())).mod(p);
+        	
+        	if(yAddition == BigInteger.ZERO)
+            	return Point.INFINITY_POINT;
+            
+            numerator = point1.GetX().multiply(point1.GetX()).multiply(BigInteger.valueOf(3)).add(a);
+            denominator = point1.GetY().add(point1.GetY());
+        }
+        
+        else {
+            numerator = point2.GetY().subtract(point1.GetY());
+            denominator = point2.GetX().subtract(point1.GetX());
+        }
+        
+        lambda = numerator.multiply(denominator.modInverse(p));
+        BigInteger newXCoordinate = CalculateXCoordinate(lambda, point1, point2);
 		BigInteger newYCoordinate = CalculateYCoordinate(lambda, point1, newXCoordinate);
-		
-		return new Point(newXCoordinate,
-						 newYCoordinate,
-						 BigInteger.ONE);
-	}
+        
+        return new Point(newXCoordinate,
+                    newYCoordinate,
+                    BigInteger.ONE);
+    }
 	
 	Point EllipticCurvePointMultiplication(Point p, int n) {
 		Point result = Point.INFINITY_POINT;
@@ -68,12 +68,10 @@ class EllipticCurve {
 	}
 	
 	private BigInteger CalculateXCoordinate(BigInteger lambda, Point p1, Point p2) {
-		BigInteger subtraction = p1.GetX().add(p2.GetX().negate());
-		return lambda.multiply(lambda).add(subtraction.negate()).mod(p);
+		return lambda.multiply(lambda).subtract(p1.GetX()).subtract(p2.GetX()).mod(p);
 	}
 	
 	private BigInteger CalculateYCoordinate(BigInteger lambda, Point p1, BigInteger x) {
-		BigInteger subtraction = p1.GetX().add(x.negate());
-		return ((lambda.multiply(subtraction)).add(p1.GetY().negate())).mod(p);
-	}	
+		return lambda.multiply(p1.GetX().subtract(x)).subtract(p1.GetY()).mod(p);
+	}
 }
